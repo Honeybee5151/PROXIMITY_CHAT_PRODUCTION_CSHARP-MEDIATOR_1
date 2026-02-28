44311742f28902822edcb2b521a73298ee681472
Honeybee5151 <milessunhede258@gmail.com>
2026-02-28 08:36:24 +0100

    awd

--- obj/Release/net8.0/win-x64/ConsoleApp1.AssemblyInfo.cs
+++ obj/Release/net8.0/win-x64/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf103d71e51aa0041d22255b2b1736aafa1b8a89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cc17556e22ee47a3734958748342635070dca94")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
